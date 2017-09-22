24b3dcf98a5cd1229ebb93eb72e9583fcf091e73
r0tiart <antley.chu@gmail.com>
2017-09-22 12:12:24 -0400

    adding upload img to userworksshow

--- src/containers/UserWorksShow.js
+++ src/containers/UserWorksShow.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import WorksList from '../components/WorksList'
+import FileBase64 from 'react-file-base64';
 
 
 class UserWorksShow extends Component {
+	constructor(props) {
+    	super(props);
+
+	    this.state = {
+	      username: '',
+	      password: '',
+	      email: '',
+	      avatar: ''
+	    };
+
+    this.onDrop = this.onDrop.bind(this);
+
+  }
+
+    onDrop(picture) {
+        this.setState({
+            avatar: picture,
+        });
+    }
 
 
 	render(){
@@ -13,9 +33,16 @@ class UserWorksShow extends Component {
 		  <div>
 		    <h3>Username: { user.username }</h3>
 		     <h3>Title: { work.title }</h3>
-		     
+
    			<p>Descriptin: { work.description } </p>
     		<span>Category: { work.category } </span>
+
+    		<div>
+	          <h5>Upload Avatar</h5>
+	          <FileBase64
+	            multiple={ false }
+	            onDone={ this.onDrop.bind(this) } />
+        	</div>
 		  </div>	
 		);
 	};
