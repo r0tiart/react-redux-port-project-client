bed9cddb021895eb823094622c04cc5556b6a9cb
r0tiart <antley.chu@gmail.com>
2017-09-21 21:43:40 -0400

    created blank files to represent works

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 
 const UserProfile = ({ user }) => {
-  // const renderProfile = users.map(user => 
-  //   <Link style={{ marginRight: '12px' }} key={user.id} to={`/users/${user.id}`}>{user.username}</Link>
-  // );
+
   return (
     <div>
     <h1>{user.username}'s Profile Page</h1>
