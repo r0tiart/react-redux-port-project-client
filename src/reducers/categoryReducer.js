1c43cc47b3a4484118da8a20a827e42de3da682c
r0tiart <antley.chu@gmail.com>
2017-09-23 19:16:28 -0400

    added a category reducer and merged it into index

---
+++ src/reducers/categoryReducer.js
@@ -0,0 +1,9 @@
+export default function categoriesReducer(state = { categories: [] }, action) {
+  switch (action.type) {
+  
+    case 'FETCH_CATEGORIES':
+      return Object.assign( {}, state, { categories: action.payload.categories } );
+    default:
+      return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './userReducer.js';
-import worksReducer from './workReducer.js'
+import worksReducer from './workReducer.js';
+import categoriesReducer from './categoryReducer.js';
 
 const rootReducer =  combineReducers({
   users: usersReducer,
-  works: worksReducer
+  works: worksReducer,
+  categories: categoriesReducer
 });
 
 export default rootReducer;
