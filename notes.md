298ca9e774eea7b22ad6923925a07a27a8d37695
r0tiart <antley.chu@gmail.com>
2017-09-28 00:24:30 -0400

    non persistent click feature

---
+++ notes.md
@@ -0,0 +1,4 @@
+1. Finish Click feature without persistence/redux - separate branch
+2. Add rails persistence to Click feature - separate branch
+
+meetme.so/reactlearnassessment
--- src/components/WorksList.js
+++ src/components/WorksList.js
@@ -1,32 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
 
-const WorksLists = ({ works, id  }) => {
 
-	if(id){
 
-	  var renderWorks = works.map(work => 
-	  	<ListGroupItem key={work.id}>
-	   		 <Link style={{ marginRight: '12px' }}  to={ `/users/${ id }/works/${ work.id }` }>{ work.title }</Link>
-	   	</ListGroupItem>
-	     );
-	} else {
+export default class WorksLists extends Component {
 
-	  var renderWorks = works.map(work => 
-	  	<ListGroupItem key={work.id}>
-	   		 <Link style={{ marginRight: '12px' }}  to={ `/works/${ work.id }` }>{ work.title }</Link>
-	   	</ListGroupItem>
-	     );
-    }
+	constructor(props) {		
+		super(props)
 
-  return (
-    <div>
-    	<ListGroup>
-      		{ renderWorks }
-      	</ListGroup>
-    </div>
-  );
+		var states = {};
+
+		this.props.works.forEach( function(element) {		
+			states[`number_${element.id}`] = 0
+		})
+
+		this.state = states
+		console.log(this.state)
+	}
+
+
+
+	handleClick = (event) => {
+				
+	  this.setState({
+        [event.target.id]: this.state[event.target.id] + 1       
+      });
+	}
+
+	
+	render(){
+		const { works, id } = this.props
+
+		if(id){
+
+		  var renderWorks = works.map(work => 
+		  	<ListGroupItem key={work.id}>
+		  		 let n
+		   		 <Link style={{ marginRight: '12px' }}  to={ `/users/${ id }/works/${ work.id }` }>{ work.title }</Link>
+		   		 <button id={`number_${work.id}`} type="button" onClick={ this.handleClick.bind(this) }>Click Me!</button> <span>{ this.state[`number_${work.id}`] }</span>
+		   	</ListGroupItem>
+		     );
+		} else {
+
+		  var renderWorks = works.map(work => 
+		  	<ListGroupItem key={work.id}>
+		   		 <Link style={{ marginRight: '12px' }}  to={ `/works/${ work.id }` }>{ work.title }</Link>
+		   		 <button id={`number_${work.id}`} type="button" onClick={ this.handleClick.bind(this) }>Click Me!</button> <span>{ this.state[`number_${work.id}`] }</span>
+		   	</ListGroupItem>
+		     );
+	    }
+
+
+    
+		return (
+		<div>
+			<ListGroup>
+				{ renderWorks }
+			</ListGroup>
+		</div>
+		);
+	}  
 };
 
-export default WorksLists;
